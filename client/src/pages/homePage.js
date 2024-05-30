f04d2f667dbe6a9e039cb6edae8d792b9e861adb
shreejana maharjan <shreejana.maharjan61@gmail.com>
2024-05-30 12:13:54 +0545

    client update

--- client/src/pages/homePage.js
+++ client/src/pages/homePage.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const homePage = () => {
+const HomePage = () => {
   return (
     <div>
       <h1>Home Page</h1>
@@ -8,4 +8,4 @@ const homePage = () => {
   )
 }
 
-export default homePage
+export default HomePage;
