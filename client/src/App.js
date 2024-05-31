d304e694bbd50dd67b9d703a003a8c7883897c83
shreejana maharjan <shreejana.maharjan61@gmail.com>
2024-05-31 14:11:43 +0545

    login frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,27 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import Login from "./pages/Login";
-import Register from "./pages/Register";
+// import Register from "./pages/Register";
+// import ToggleContainer from "./pages/ToggleContainer";
 
-function App() {
+
+
+function App() { 
   return (
     <>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
+          {/* <Route path="/register" element={<Register />} /> */}
+         
         </Routes>
       </BrowserRouter>
     </>
   );
 }
+
+
+
 
 export default App;
