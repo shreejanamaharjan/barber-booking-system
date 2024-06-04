ac58d52361945f1d522638112015c3b31d7b15ff
shreejana maharjan <shreejana.maharjan61@gmail.com>
2024-06-04 23:13:25 +0545

    frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,27 +1,21 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
 import HomePage from "./pages/HomePage";
 import Login from "./pages/Login";
-// import Register from "./pages/Register";
-// import ToggleContainer from "./pages/ToggleContainer";
+import Register from "./pages/Register";
 
-
-
-function App() { 
+function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/login" element={<Login />} />
-          {/* <Route path="/register" element={<Register />} /> */}
-         
+          <Route path="/register" element={<Register/>} />
         </Routes>
       </BrowserRouter>
     </>
   );
-}
-
-
-
+};
 
 export default App;
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 const HomePage = () => {
   return (
     <div>
-      <h1>Home Page</h1>
+      <h1>homepage</h1>
     </div>
   )
 }
 
-export default HomePage;
+export default HomePage
