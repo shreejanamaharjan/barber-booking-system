a62a0e157fcc2934d659ecb56caf8049a1890279
Crotalus-atrox <crestacreeya740@gmail.com>
2024-06-11 15:38:41 +0545

    slider

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -47,15 +47,19 @@ const HomePage = () => {
       <div className="slider-container">
         <Slider {...settings}>
           <div>
-            <img src="C:\Users\kabir\OneDrive\Desktop\barber-booking-system\frontend\src\slider images\slide1.jpg" alt="Slide 1" />
+            <img src="./src/slider images/slide1.jpg" alt="Slide 1" />
           </div>
           <div>
-            <img src="C:\Users\kabir\OneDrive\Desktop\barber-booking-system\frontend\src\slider images\slide2.webp" alt="Slide 2" />
+            <img src="./src/slider images/slide2.jpg" alt="Slide 2" />
           </div>
           <div>
-            <img src="C:\Users\kabir\OneDrive\Desktop\barber-booking-system\frontend\src\slider images\slide3.jpg" alt="Slide 3" />
+            <img src="./src/slider images/slide3.jpg" alt="Slide 3" />
           </div>
         </Slider>
+        <div className= "description">
+          <h1 className='header'>Find the best Barber Shop</h1>
+           <p>Where style meets precision.<br />We believe that a great haircut is more than just a trim – it's an experience. Our platform is a heaven for those who value style, precision, and top-notch service with few clicks.We healp you find skilled barbers dedicated to delivering personalized grooming solutions that leave you looking sharp and feeling confident.</p>
+        </div>
       </div>
     </Layout>
   );
