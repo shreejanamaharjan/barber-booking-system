99004e630bed33174f3d122373d69bb02f6b5aa1
shreejana maharjan <shreejana.maharjan61@gmail.com>
2024-06-12 11:15:36 +0545

    login, register, contact frontend

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,45 +1,73 @@
-import React from 'react'
+import React, { useState, useContext }from 'react'
 import '../styles/RegisterStyles.css'
 import {Button, Form, Input, message} from 'antd'
 import {Link, useNavigate} from 'react-router-dom'
 import axios from 'axios'
 
 const Login = () => {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+  
+  const handleInputChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
 
-  const navigate = useNavigate()
-   // form handler
-   const onfinishhandler = async(values) =>{
-    try{
-      const res = await axios.post('api/v1/user/login', values)
-      if(res.data.success){
-        localStorage.setItem("token", res.data.token)
-        message.success('Login successfully')
-        navigate('/')
-      }else{
-        message.error(res.data.message)
-      }
-    }
-    catch(error){
-      console.log(error)
-      message.error('Something went wrong')
-    }
-  }
+  
   return (
-    <div className="form-container card">
-        <Form layout='vertical' onFinish={onfinishhandler} className='register-form'>
-          <h2 className='text-center'>Login Form</h2>
-          <Form.Item name="email">
-            <Input type="email" placeholder="Email" required></Input>
-          </Form.Item>
-          <Form.Item name="password">
-            <Input type="password" placeholder="Password" required></Input>
-          </Form.Item>
-          <Link to="/" className='m-2' >Forgot password?</Link>
-          <Link to="/register" className='m-2' >Create account</Link>
-          <button className='btn btn-primary' type='submit'>Login</button>
-        </Form>
-    </div>
+    <section className="px-5 lg:px-0 flex items-center justify-center">
+      <div className="rounded-lg shadow-md md:p-10 text-center">
+        <h3 className="text-headingColor text-2xl lg:text-3xl leading-9 font-bold mb-10">
+          Hello! <span className="text-primaryColor">Welcome</span> Back
+        </h3>
+        <form className="py-4 md:py-0" >
+          <div className="mb-5">
+            <input
+              type="email"
+              placeholder="Enter your email"
+              name="email"
+              value={formData.email}
+              onChange={handleInputChange}
+              className="w-full py-3 border-b border-solid border-[#35727B] focus:outline-none focus:border-b-primaryColor text-lg lg:text-[15px] leading-5 text-headingColor placeholder:text-textColor cursor-pointer"
+              required
+              aria-label="Email"
+            />
+          </div>
+
+          <div className="mb-5">
+            <input
+              type="password"
+              placeholder="Password"
+              name="password"
+              value={formData.password}
+              onChange={handleInputChange}
+              className="w-full py-3 border-b border-solid border-[#35727B] focus:outline-none focus:border-b-primaryColor text-lg lg:text-[15px] leading-5 text-headingColor placeholder:text-textColor cursor-pointer"
+              required
+              aria-label="Password"
+            />
+          </div>
+
+          <div className="mt-9">
+            <button
+              type="submit"
+              className="w-full bg-primaryColor text-white text-[18px] leading-[3px] rounded-lg px-4 py-3"
+            >
+              Login
+            </button>
+
+            <p className="mt-5 text-textColor text-center">
+            Don't have an account?
+            <Link to="/register" className="text-primaryColor font-medium ml-1">
+              Register
+            </Link>
+          </p>
+          </div>
+        </form>
+      </div>
+    </section>
+    
   )
-}
+};
 
 export default Login
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,45 +1,142 @@
-import React from 'react'
-import '../styles/RegisterStyles.css'
-import {Button, Form, Input, message} from 'antd'
-import axios from 'axios'
-import {Link, useNavigate} from 'react-router-dom'
+import React, { useState, useContext } from "react";
+import "../styles/RegisterStyles.css";
+import { Button, Form, Input, message } from "antd";
+import axios from "axios";
+import signupImg from "../assets/images/signup.png";
+import { Link, useNavigate } from "react-router-dom";
 const Register = () => {
-  const navigate = useNavigate()
-  // form handler
-  const onfinishhandler = async(values) =>{
-    try{
-      const res = await axios.post('api/user/register', values)
-      if(res.data.success){
-        message.success('register successfully')
-        navigate('/login')
-      }else{
-        message.error('something went wrong');
-      }
-    }catch(error){
-      console.log(error)
-      message.error('something went wrong')
-    }
-  }
+  const [selectedFile, setSelectedFile] = useState(null);
+  const [previewURL, setpreviewURL] = useState("");
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    gender: "male",
+    role: "customer",
+  });
+
+  const handleInputChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleFileInputChange = async (event) => {
+    const file = event.target.files[0];
+
+    console.log(file);
+  };
+  const submitHandler = async (event) => {
+    event.preventDefault();
+  };
   return (
-    <>
-    <div className="form-container card">
-        <Form layout='vertical' onFinish={onfinishhandler} className='register-form'>
-          <h2 className='text-center'>Register Form</h2>
-          <Form.Item name="name">
-            <Input type="text" placeholder="Name" required></Input>
-          </Form.Item>
-          <Form.Item name="email">
-            <Input type="email" placeholder="Email" required></Input>
-          </Form.Item>
-          <Form.Item name="password">
-            <Input type="password" placeholder="Password" required></Input>
-          </Form.Item>
-          <Link to="/login" className='m-2' >Already have an account?</Link>
-          <button className='btn btn-primary' type='submit'>Register</button>
-        </Form>
-    </div>
-    </>
-  )
-}
+    <section className="px-5 xl:px-0">
+      <div className="max-w-[1170px] mx-auto">
+        <div className="grid grid-cols-1 lg:grid-cols-2">
+          {/* image box */}
+          <div className="hidden lg:block rounded-l-lg">
+            <figure className="rounded-l-lg">
+              <img
+                src={signupImg}
+                alt=""
+                className=" rounded-l-lg "
+                height={150}
+                width={400}
+              />
+            </figure>
+          </div>
+
+          {/* signup form */}
+          <div className="rounded-l-lg lg:pl-16 py-10">
+            <h3 className="text-headingColor text-[22px] leading-9 font-bold mb-10">
+              Create an <span className="text-primaryColor">account</span>
+            </h3>
+
+            <form onSubmit={submitHandler}>
+              <div className="mb-3">
+                <input
+                  type="text"
+                  placeholder="Full Name"
+                  name="name"
+                  value={formData.name}
+                  onChange={handleInputChange}
+                  className="w-full pr-4 py-3 border-b border-solid border-[#35727B] focus:outline-none focus:border-b-primaryColor text-lg lg:text-[13px] leading-3 text-headingColor placeholder:text-textColor cursor-pointer"
+                  required
+                />
+              </div>
+              <div className="mb-3">
+                <input
+                  type="email"
+                  placeholder="Enter your email"
+                  name="email"
+                  value={formData.email}
+                  onChange={handleInputChange}
+                  className="w-full pr-4  py-3 border-b border-solid border-[#35727B] focus:outline-none focus:border-b-primaryColor text-lg lg:text-[15px] leading-5 text-headingColor placeholder:text-textColor cursor-pointer"
+                  required
+                />
+              </div>
+              <div className="mb-3">
+                <input
+                  type="password"
+                  placeholder="Password"
+                  name="password"
+                  value={formData.password}
+                  onChange={handleInputChange}
+                  className="w-full pr-4  py-3 border-b border-solid border-[#35727B] focus:outline-none focus:border-b-primaryColor text-lg lg:text-[13px] leading-3 text-headingColor placeholder:text-textColor cursor-pointer"
+                  required
+                />
+              </div>
+              <div className="mb-3 flex items-center justify-between">
+                <label className="text-headingColor font-bold text-[13px] leading-3">
+                  Are you a:
+                  <select
+                    name="role"
+                    value={formData.role}
+                    onChange={handleInputChange}
+                    className="text-textColor font-[400] text-[13px] leading-3 px-4 py-3 focus:outline-none"
+                  >
+                    <option value="barber">Barber</option>
+                    <option value="customer">Customer</option>
+                  </select>
+                </label>
+
+                <label className="text-headingColor font-bold text-[13px] leading-3">
+                  Gender:
+                  <select
+                    name="gender"
+                    value={formData.gender}
+                    onChange={handleInputChange}
+                    className="text-textColor font-[400] text-[13px] leading-3 px-4 py-3 focus:outline-none"
+                  >
+                    <option value="male">Male</option>
+                    <option value="female">Female</option>
+                    <option value="other">Other</option>
+                  </select>
+                </label>
+              </div>
+
+              <div className="mt-3">
+                <button
+                  type="submit"
+                  className="w-full bg-primaryColor text-white text-[16px] leading-[3px] rounded-lg px-4 py-3"
+                >
+                  Sign Up
+                </button>
+              </div>
+
+              <p className="mt-3 text-textColor text-center">
+                Already have an account?
+                <Link
+                  to="/login"
+                  className="text-primaryColor font-medium ml-1"
+                >
+                  Login
+                </Link>
+              </p>
+            </form>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
 
 export default Register;
