0930e614cd9d1e3c19d259d2238eabcc887aa315
ashila khadka <khadkaashila7@gmail.com>
2024-07-12 14:50:08 +0545

    contact section

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -5,7 +5,7 @@ const Contact = () => {
   return (
     <section>
       <div className="px-4 mx-auto max-w-screen-md">
-        <h2 className="heading text-center text-primaryColor">Contact Us</h2>
+        <h2 className="heading text-center text-primaryColor">Contact Us</h2> {/*colour*/}
         <p className="mb-8 lg:mb-16 font-light text-center text__para">
           Got a technical issue? Or want to give us feedback? We are here to
           help you.
